817efd99e0564e576688c65670e225fef2b554c1
Pico143 <pico19942@gmail.com>
2018-04-24 14:53:26 +0200

    fixed refreshing so that F5 does not send a form again

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET","POST"])
+@app.route("/")
 def boards():
     ''' this is a one-pager which shows all the boards and cards '''
     return render_template('boards.html')
 
+@app.route("/new-board", methods=["POST"])
+def new_board():
+    return redirect("/")
 
 def main():
     app.run(debug=True)
